8d8520426ba7442f8f13b4a4f7041f680a338834
Yvan RAZAFINDRAMANANA <mail@ludeo.net>
2026-04-17 17:57:45 +0200

    Bootstrap

---
+++ front/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  protected readonly title = signal('voyagevoyage-front');
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
