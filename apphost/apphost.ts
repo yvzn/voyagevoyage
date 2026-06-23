83c2ce19779c5ada5f3e0857d43cebab52e8a68c
Yvan RAZAFINDRAMANANA <mail@ludeo.net>
2026-06-23 12:00:30 +0200

    More CosmosDB cleanup

--- apphost/apphost.ts
+++ apphost/apphost.ts
@@ -6,11 +6,7 @@ import { createBuilder } from './.modules/aspire.js';
 const builder = await createBuilder();
 
 // Add your resources here:
-let cosmos = await builder.addAzureCosmosDB('cosmosdb')
-    .runAsEmulator();
-
-await builder.addCSharpApp('server', '../server/VoyageVoyage.Server/VoyageVoyage.Server.csproj')
-    .withReference(cosmos);
+await builder.addCSharpApp('server', '../server/VoyageVoyage.Server/VoyageVoyage.Server.csproj');
 await builder.addJavaScriptApp('front', '../front')
     .withUrl("http://localhost:4200")
     .withRunScript('start');
